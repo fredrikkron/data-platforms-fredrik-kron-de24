21509463a8cbc72e652aa522f204b90653c51279
fredrikkron <fkron95@gmail.com>
2025-01-28 10:53:25 +0100

    additional work on producer/consumer

--- code_alongs/08_producer_consumer/src/consumer.py
+++ code_alongs/08_producer_consumer/src/consumer.py
@@ -10,7 +10,19 @@ jokes_topic = app.topic(name="jokes", value_deserializer="json")
 
 sdf = app.dataframe(topic=jokes_topic)
 
+# def print_message(message):
+    # print(f"Input: {message})            different ways of writing
+# sdf.update(print_message)
 sdf = sdf.update(lambda message: print(f"Input: {message}"))
+
+# transformations
+# split the words
+
+# def split_words(message):
+#     return [word for word in message["joke_text"].split()]
+
+# sdf = sdf.apply(split_words, expand=True)
+# sdf.update(lambda words: print(f"Output: {words})) 
 
 sdf = sdf.apply(
     lambda message: [{"word": word} for word in message["joke_text"].split()],
--- code_alongs/08_producer_consumer/src/producer.py
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -7,11 +7,13 @@ data_path = Path(__file__).parents[1] / "data"
 
 # print(data_path)
 
+# reads in jokes.json through its absolute path
 with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
 
 # pprint(jokes)
 
+# a form of entry point for interacting with Kafka, localhost:9092 is port mapped to broker container
 app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
 
 jokes_topic = app.topic(name="jokes", value_serializer="json")
