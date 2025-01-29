8bf4fb0cc882a2164d4e8534ffd31c7af4674f09
fredrikkron <fkron95@gmail.com>
2025-01-29 10:58:07 +0100

    load file in producer

--- exercises/exercise_3.2/src/producer.py
+++ exercises/exercise_3.2/src/producer.py
@@ -1,3 +1,14 @@
 from pathlib import Path
 import json
+from pprint import pprint
 from quixstreams import Application
+
+data_path = Path(__file__).parents[1] / "data"
+
+print(data_path)
+
+with open(data_path / "orders.json", "r") as file:
+    orders = json.load(file)
+
+# pprint(orders)
+# print(type(orders))
