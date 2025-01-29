0b4d6d6f6a9f908f45210d4f31e2b2c6026fec8d
fredrikkron <fkron95@gmail.com>
2025-01-29 15:03:25 +0100

    producer exercise 3_2

--- exercises/exercise_3_2/src/producer.py
+++ exercises/exercise_3_2/src/producer.py
@@ -5,10 +5,21 @@ from quixstreams import Application
 
 data_path = Path(__file__).parents[1] / "data"
 
-print(data_path)
-
 with open(data_path / "orders.json", "r") as file:
     orders = json.load(file)
 
-# pprint(orders)
-# print(type(orders))
+app = Application(broker_address="localhost:9092", consumer_group="show-orders")
+
+orders_topic = app.topic(name="orders", value_serializer="json")
+
+def main():
+    with app.get_producer() as producer:
+        for order in orders:
+            kafka_msg = orders_topic.serialize(key=order["order_id"], value=order['products'])
+            print(f"Order number: {kafka_msg.key} \nProducts: {kafka_msg.value}")
+            print()
+
+            producer.produce(topic="orders", key=kafka_msg.key, value=kafka_msg.value)
+
+if __name__ == "__main__":
+    main()
