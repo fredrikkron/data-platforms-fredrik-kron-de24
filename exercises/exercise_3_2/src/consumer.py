98f12bd111780ae9132a9382bf743633f1039286
fredrikkron <fkron95@gmail.com>
2025-01-30 16:15:24 +0100

    exploring exercise

--- exercises/exercise_3_2/src/consumer.py
+++ exercises/exercise_3_2/src/consumer.py
@@ -1,5 +1,4 @@
 from quixstreams import Application
-import json
 
 app = Application(
     broker_address="localhost:9092",
@@ -12,6 +11,24 @@ orders_topic = app.topic(name="orders", value_deserializer="json")
 sdf = app.dataframe(topic=orders_topic)
 
 sdf = sdf.update(lambda message: print(f"Input: {message}"))
+
+# transform the message
+def print_order_details(order):
+    print(f"Order ID: {order['order_id']}")
+    total_price = 0
+    for product in order['products']:
+        product_name = product['name']
+        quantity = product['quantity']
+        price = product['price']
+        total = quantity * price
+        total_price += total
+        print(f"Product: {product_name:<20} Quantity: {quantity:<5} Price: {price:<10.2f}")
+    print(f"Total price: {total_price:.2f}\n")
+
+
+sdf = sdf.apply(print_order_details, expand=True)
+
+sdf.update(lambda order_details: print (f"Output: {order_details}"))
 
 
 if __name__ == '__main__':
