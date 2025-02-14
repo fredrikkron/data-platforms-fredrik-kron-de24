cd204f70fb252569aa1246635b8974acfb6a2090
fredrikkron <fkron95@gmail.com>
2025-02-14 10:22:26 +0100

    code along dashboard completed

--- code_alongs/13_live_dashboard/src/dashboard.py
+++ code_alongs/13_live_dashboard/src/dashboard.py
@@ -8,10 +8,14 @@ from constants import (
 )
 from sqlalchemy import create_engine
 import pandas as pd
+from charts import line_chart
+from streamlit_autorefresh import st_autorefresh
 
 connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
 
 engine = create_engine(connection_string)
+
+count = st_autorefresh(interval=1000*10, limit=100)
 
 def load_data(query):
     with engine.connect() as conn:
@@ -29,6 +33,21 @@ def layout():
     st.markdown("Latest data directly from postgres database in a docker container")
 
     st.dataframe(df.tail())
+
+    st.markdown("## Bitcoin latest price in USD")
+
+    # plotting code
+
+    price_chart = line_chart(
+        df["timestamp"],
+        df["price_usd"],
+        title="price USD for bitcoin",
+        xlabel="time",
+        ylabel="USD",
+    )
+
+    st.pyplot(price_chart)
+
 
 if __name__ == "__main__":
     layout()
